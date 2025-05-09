88f24af1ab3230a560522101ec7ce3e927e0f87a
BadCodeBloke// <270663260@yoobeestudent.ac.nz>
2025-05-09 19:52:30 +1200

    Temperature.py

--- WEEK4/ACTIVITY1_WEATHER/CompareTemp.py
+++ WEEK4/ACTIVITY1_WEATHER/CompareTemp.py
@@ -1,17 +1,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-class TempComparison:
+class TemperatureData:
     def __init__(self):
         self.auckland = None
         self.christchurch = None
         self.combined = None
 
     def load_data(self):
-        self.auckland = pd.read_csv('ACTIVITY1_WEATHER\auckland_temperature.csv')
+        # full paths to the CSV uploaded files
+        self.auckland = pd.read_csv('WEEK4/ACTIVITY1_WEATHER/auckland_temperature.csv')
         self.auckland['City'] = 'Auckland'
 
-        self.christchurch = pd.read_csv('ACTIVITY1_WEATHER\christchurch_temperature.csv')
+        self.christchurch = pd.read_csv('WEEK4/ACTIVITY1_WEATHER/christchurch_temperature.csv')
         self.christchurch['City'] = 'Christchurch'
 
         self.combined = pd.concat([self.auckland, self.christchurch])
@@ -30,7 +31,6 @@ class TempComparison:
         plt.tight_layout()
         plt.show()
 
-# Run the program
-comparison = TempComparison()
+comparison = TemperatureData()
 comparison.load_data()
 comparison.show_plot()
